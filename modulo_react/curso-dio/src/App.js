28b71f14d52d9bc25073c198eac708c56d37eb99
anderson <andmat7745@gmail.com>
2026-05-29 16:24:14 -0300

    react e desafios

---
+++ modulo_react/curso-dio/src/App.js
@@ -0,0 +1,13 @@
+import Button from './components/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, mundo!</h1>
+      <Button title="Clique aqui" />
+    </div>
+  );
+}
+
+export default App;
