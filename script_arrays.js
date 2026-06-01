febf501ab111dcb13314225857be2947384e96c7
anderson <andmat7745@gmail.com>
2026-06-01 17:11:02 -0300

    inicio do desafio 01 calculadora

--- script_arrays.js
+++ script_arrays.js
@@ -41,3 +41,15 @@ console.log(temAlunosMenoresDe10)
 //every: verifica se todos os elementos do array satisfazem a condição
 const todosAlunosMaioresDe15 = alunos.every(aluno => aluno.idade >= 9)
 console.log(todosAlunosMaioresDe15)
+
+
+var array = [ 2, 3, 5, 7, 11, 13, 18, 34 ];
+
+//TODO: Complete o laço de repetição:
+for (let i = 0; i < array.length; i++){
+  
+//TODO: Agora crie uma condição que verifique o ARRAY e imprima APENAS os números pares.
+  if (array[i] % 2 === 0) {
+    console.log(array[i]);
+  }
+}
