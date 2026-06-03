84019461bc2374c04312b754b0bbd15f7513f028
anderson <andmat7745@gmail.com>
2026-06-03 11:23:55 -0300

    conclusão desafio calculadora

---
+++ desafio01-calculadora/src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    font-size: 20px;
+    font-weight: 700;
+    flex: 1;
+    color: #FFFFFF;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAFF;
+
+    &:hover {
+    opacity: 0.8;
+        }
+`
---
+++ desafio01-calculadora/src/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #CACACA;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+`
+
+export const Content = styled.div`
+    background-color: #ffff;
+    width: 16.5%;
+    
+        
+`
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;    
+    padding: 1.5px;   
+    justify-content: space-between;
+    align-items: center;
+`
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`
+;
